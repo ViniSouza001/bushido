f03b7e59652106f521330a3ae9246803ca70f605
ViniSouza001 <viniciussouza130705@gmail.com>
2023-10-09 11:19:04 -0300

    bushido

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const setaHome = document.querySelectorAll(".seta-baixo")
 var nextPage = 0
+const form = document.querySelector('form')
 const home = document.querySelector('.home')
 const divsEsquerdas = document.querySelector('.divs-esquerdas')
 const divsDireitas = document.querySelector('.divs-direitas')
@@ -12,10 +13,35 @@ if (nextPage == 1) {
 
 }
 
-function abrirModal (element) {
-   element.style.animation = 'abrir-modal 1.5s forwards'
-   const paragrafoFilho = element.querySelector('#paragrafoFilho')
-   paragrafoFilho.style.animation = 'fadeout 1s forwards'
+function abrirModalEsquerdo (element) {
+   element.classList.add('abrir-modal-esquerdo')
+   const paragrafoFilho = element.querySelector('.paragrafoFilho')
+   paragrafoFilho.style.animation = 'fadeout .5s forwards'
+   const conteudo = element.querySelector('.divs-modal')
+   setTimeout(() => {
+      conteudo.classList.add('fadein-conteudo')
+   }, 1200)
+
+   const x = conteudo.querySelector('.fecharModal')
+   x.addEventListener('click', () => {
+      element.classList.remove("abrir-modal-esquerdo")
+      element.classList.add('fechar-modal-esquerdo')
+      conteudo.classList.add('fadeout-conteudo')
+      setTimeout(() => {
+         paragrafoFilho.classList.add('fadein')
+      })
+   })
+}
+
+function fecharModalEsquerdo(element) {
+   console.log(element)
+   element.style.animation = 'fechar-modal-esquerdo 1s forward'
+}
+
+function abrirModalDireito(element) {
+   element.style.animation = 'abrir-modal-direito 1.5s forwards'
+   const paragrafoFilho = element.querySelector('.paragrafoFilho')
+   paragrafoFilho.style.animation = 'fadeout .5s forwards'
    const conteudo = element.querySelector('.divs-modal')
    setTimeout(() => {
       conteudo.style.display = 'flex'
@@ -23,8 +49,8 @@ function abrirModal (element) {
    }, 1200)
 }
 
-function changeNextPage () {
-   nextPage++
+function changeNextPage (value) {
+   nextPage = nextPage + value
    if (nextPage == 1) {
       home.style.animation = 'fadeout-up 1s'
       home.style.animationFillMode = 'both'
