38df3bcf60d4b4c061f57ed5fd7ec2d7e4705d65
ViniSouza001 <viniciussouza13705@gmail.com>
2023-10-11 10:22:38 -0300

    bushido

--- script.js
+++ script.js
@@ -10,59 +10,64 @@ if (nextPage === 0) {
 }
 
 const abrirModalEsquerdo = (element) => {
-   if(element.className == 'topicosEsquerdos') {
-      // console.log(element)
-      element.classList.add('abrir-modal-esquerdo')
-      const paragrafoFilho = element.querySelector('.paragrafoFilho')
-      paragrafoFilho.classList.add('fadeout-paragrafo')
-      const conteudo = element.querySelector('.divs-modal')
-      setTimeout(() => {
-         conteudo.classList.add('fadein-conteudo')
-      }, 1200)
-
-      const x = conteudo.querySelector('.fecharModalEsquerdo')
-      
-      x.addEventListener('click', () => {
-         conteudo.classList.add('fadeout-conteudo')
-         setTimeout(() => {
-            element.classList.add('fechar-modal-esquerdo')
-         }, 300);
-         setTimeout(() => {
-            paragrafoFilho.classList.remove('fadeout-paragrafo')
-            paragrafoFilho.classList.add('fadein-paragrafo')
-         }, 1000)
-         setTimeout(() => {
-            element.classList.remove("abrir-modal-esquerdo")
-            conteudo.classList.remove('fadein-conteudo')
-            conteudo.classList.remove('fadeout-conteudo')
-            element.classList.remove('fechar-modal-esquerdo')
-         }, 2000)
-      })
-   } else {
-      console.log(element)
-      // element.classList.add('abrir-modal-direito')
-      console.log('direito')
-
-   }
-}
-
-// function fecharModalEsquerdo (element) {
-//    console.log(element)
-//    element.style.animation = 'fechar-modal-esquerdo 1s forward'
-// }
-
-function abrirModalDireito (element) {
-   element.style.animation = 'abrir-modal-direito 1.5s forwards'
+   element.classList.add('abrir-modal-esquerdo')
    const paragrafoFilho = element.querySelector('.paragrafoFilho')
-   paragrafoFilho.style.animation = 'fadeout .5s forwards'
+   paragrafoFilho.classList.add('fadeout-paragrafo')
    const conteudo = element.querySelector('.divs-modal')
    setTimeout(() => {
-      conteudo.style.display = 'flex'
-      conteudo.style.animation = 'fadein 1s forwards'
+      conteudo.classList.add('fadein-conteudo')
    }, 1200)
+
+   const x = conteudo.querySelector('.fecharModalEsquerdo')
+
+   x.addEventListener('click', () => {
+      conteudo.classList.add('fadeout-conteudo')
+      setTimeout(() => {
+         element.classList.add('fechar-modal-esquerdo')
+      }, 300);
+      setTimeout(() => {
+         paragrafoFilho.classList.remove('fadeout-paragrafo')
+         paragrafoFilho.classList.add('fadein-paragrafo')
+      }, 1000)
+      setTimeout(() => {
+         element.classList.remove("abrir-modal-esquerdo")
+         conteudo.classList.remove('fadein-conteudo')
+         conteudo.classList.remove('fadeout-conteudo')
+         element.classList.remove('fechar-modal-esquerdo')
+      }, 2000)
+   })
 }
 
-function changeNextPage (value) {
+function abrirModalDireito(element) {
+   element.classList.add('abrir-modal-direito')
+   const paragrafoFilho = element.querySelector('.paragrafoFilho')
+   paragrafoFilho.classList.add('fadeout-paragrafo')
+   const conteudo = element.querySelector('.divs-modal')
+   setTimeout(() => {
+      conteudo.classList.add('fadein-conteudo')
+   }, 1200)
+
+   const x = conteudo.querySelector('.fecharModalDireito')
+
+   x.addEventListener('click', () => {
+      conteudo.classList.add('fadeout-conteudo')
+      setTimeout(() => {
+         element.classList.add('fechar-modal-direito')
+      }, 300);
+      setTimeout(() => {
+         paragrafoFilho.classList.remove('fadeout-paragrafo')
+         paragrafoFilho.classList.add('fadein-paragrafo')
+      }, 1000)
+      setTimeout(() => {
+         element.classList.remove("abrir-modal-direito")
+         conteudo.classList.remove('fadein-conteudo')
+         conteudo.classList.remove('fadeout-conteudo')
+         element.classList.remove('fechar-modal-direito')
+      }, 2000)
+   })
+}
+
+function changeNextPage(value) {
    nextPage = nextPage + value
    if (nextPage == 1) {
       home.style.animation = 'fadeout-up 1s'
